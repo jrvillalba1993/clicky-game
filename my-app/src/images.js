2452ee2998ccb6355ae801e4d5473af6fe4d3c86
Julian Villalba <62047235+jrvillalba1993@users.noreply.github.com>
2020-09-14 18:17:26 -0400

    idkwthimdoing

---
+++ my-app/src/images.js
@@ -0,0 +1,30 @@
+import A1 from "./images/image1.jpg";
+import A2 from "./images/image2.jpg";
+import A3 from "./images/image3.jpg";
+import A4 from "./images/image4.jpg";
+import A5 from "./images/image5.jpg";
+import A6 from "./images/image6.jpg";
+import A7 from "./images/image7.jpg";
+import A8 from "./images/image8.jpg";
+import A9 from "./images/image9.jpg";
+import A10 from "./images/image10.jpg";
+import A11 from "./images/image11.jpg";
+import A12 from "./images/image12.jpg";
+
+
+const images = [
+   A1,
+   A2,
+   A3,
+   A4,
+   A5,
+   A6,
+   A7,
+   A8,
+   A9,
+   A10,
+   A11,
+   A12
+  ];
+  
+  export default images;
